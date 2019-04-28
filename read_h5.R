ffe59208302022b245ca7130cd0c83cf7b9cd013
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-04-29 00:26:09 +0300

    read Basel Model

--- read_h5.R
+++ read_h5.R
@@ -1,6 +1,7 @@
 setwd('~/Documents/Thesis - data')
 
 library(rhdf5)
+library(rgl)
 
 h5ls("data-raw/model2017-1_bfm_nomouth.h5")
 
@@ -9,3 +10,13 @@ pca_shape <- h5read("data-raw/model2017-1_bfm_nomouth.h5", "/shape/model")
 pca_color <- h5read("data-raw/model2017-1_bfm_nomouth.h5", "/color/model")
 
 pca_expression <- h5read("data-raw/model2017-1_bfm_nomouth.h5", "/expression/model")
+
+shape = h5read("data-raw/model2017-1_bfm_nomouth.h5", "/shape/representer")
+color = h5read("data-raw/model2017-1_bfm_nomouth.h5", "/color/representer")
+
+# plot
+plot3d(shape$points, col = rgb(t(color$points)))
+plot3d(shape$points, col = rgb(t(pca_color$mean)))
+
+plot3d(color[["points"]], col = rgb(pca_color$mean))
+plot3d(color[["points"]], col = rgb(t(pca_color$mean)))
