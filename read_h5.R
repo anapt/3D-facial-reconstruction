3090e8e30f9a89f64e829764a443fe86237184b8
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-05-13 12:11:30 +0300

    sampling for d,g,T

--- read_h5.R
+++ read_h5.R
@@ -33,10 +33,16 @@ plot3d(points_av[1,], points_av[2,], points_av[3,], col = rgb(t(color)))
 
 # sampling
 # sample a from a normal distribution N(0,1)
-a = rnorm(80, 0 , 1)
-d = runif(64,-24,24)
-d[1] = runif(1,-4.8,4.8)
-b = rnorm(80, 0 , 1)
+
+a = rnorm(80, 0 , 1)      #shape
+d = runif(64,-24,24)      #expression
+d[1] = runif(1,-4.8,4.8)  
+b = rnorm(80, 0 , 1)      #reflectance
+T = runif(3, -40,40)      #rotation
+T[3] = runif(1, -15,15)
+g = runif(27, -0.2,0.2)   #illumination
+g[1] = runif(1,0.6,1.2)
+
 
 # eq 2
 V = As + Es %*% a + Ee %*% d
@@ -44,19 +50,8 @@ V2 = array(V, dim = c(3, length(V)/3))
 # eq 3
 R = Ar + Er %*% b
 R2 = array(R, dim = c(3, length(R)/3))
-frame()
+
+# plot
 plot3d(V2[1,], V2[2,], V2[3,], col = rgb(t(color)))
 
-a1 = rnorm(80, 0 , 1)
-
-d1 = runif(64,-12,12)
-d1[1] = runif(1,-4.8,4.8)
-b1 = rnorm(80, 0 , 1)
-
-# eq 2
-V1 = As + Es %*% a1 + Ee %*% d1
-V21 = array(V, dim = c(3, length(V)/3))
-# eq 3
-R1 = Ar + Er %*% b1
-R21 = array(R, dim = c(3, length(R)/3))
 
