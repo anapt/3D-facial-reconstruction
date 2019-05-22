82446bada8c3ae2912076c3c4539d9e52513e9a3
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-05-22 16:32:46 +0300

    Illumination Model

--- parametricMoDecoder.py
+++ parametricMoDecoder.py
@@ -12,7 +12,7 @@ class ParametricMoDecoder:
         self.illumination = reflectance
 
     @staticmethod
-    def projection(self, coords_3d):
+    def projection(coords_3d):
 
         if abs(coords_3d[2]) > 70:
             inv_z = abs(1/coords_3d[2])
@@ -26,7 +26,7 @@ class ParametricMoDecoder:
             return np.transpose(coords_2d)
 
     @staticmethod
-    def create_rot_mat(self, a, b, c):
+    def create_rot_mat(a, b, c):
         c1 = math.cos(math.radians(c))
         c2 = math.cos(math.radians(b))
         c3 = math.cos(math.radians(a))
@@ -42,40 +42,54 @@ class ParametricMoDecoder:
         return rot_mat_so3
 
     @staticmethod
-    def transform_wcs2ccs(self, coords_ws, inv_rotmat, translation):
+    def transform_wcs2ccs(coords_ws, inv_rotmat, translation):
         coords_cs = np.dot(inv_rotmat, (coords_ws - translation))
 
         return np.transpose(coords_cs)
 
     @staticmethod
-    def get_sh_basis_function(self, x_coord, y_coord, z_coord, b):
-        r = x_coord ^ 2 + y_coord ^ 2 + z_coord ^ 2
+    def get_sh_basis_function(x_coord, y_coord, z_coord, b):
+        r = pow(x_coord, 2) + pow(y_coord, 2) + pow(z_coord, 2)
         if b == 1:
-            basis = 0.75 * math.pow(35 / math.pi, 0.5) * (x_coord * y_coord * (x_coord ^ 2 - y_coord ^ 2)) / (r ^ 4)
+            basis = 0.75 * math.pow(35 / math.pi, 0.5) * (x_coord * y_coord *
+                                                          (pow(x_coord, 2) - pow(y_coord, 2))) / (pow(r, 4))
         elif b == 2:
             basis = 0.75 * math.pow(35 / (2 * math.pi), 0.5) * \
-                 (z_coord * y_coord * (3 * x_coord ^ 2 - y_coord ^ 2)) / (r ^ 4)
+                 (z_coord * y_coord * (3 * pow(x_coord, 2) - pow(y_coord, 2))) / (pow(r, 4))
         elif b == 3:
-            basis = 0.75 * math.pow(5 / math.pi, 0.5) * (x_coord * y_coord * (7 * z_coord ^ 2 - r ^ 2)) / (r ^ 4)
+            basis = 0.75 * math.pow(5 / math.pi, 0.5) * (x_coord * y_coord *
+                                                         (7 * pow(z_coord, 2) - pow(r, 2))) / (pow(r, 4))
         elif b == 4:
             basis = 0.75 * math.pow(5 / (2 * math.pi), 0.5) * \
-                 (y_coord * z_coord * (7 * z_coord ^ 2 - 3 * r ^ 2)) / (r ^ 4)
+                 (y_coord * z_coord * (7 * pow(z_coord, 2) - 3 * pow(r, 2))) / (pow(r, 4))
         elif b == 5:
             basis = (3 / 16) * math.pow(1 / math.pi, 0.5) * \
-                 (35 * z_coord ^ 2 - 30 * z_coord ^ 2 * r ^ 2 + 3 * r ^ 4) / (r ^ 4)
+                 (35 * pow(z_coord, 2) - 30 * pow(z_coord, 2) * pow(r, 2) + 3 * pow(r, 4)) / (pow(r, 4))
         elif b == 6:
             basis = 0.75 * math.pow(5 / (2 * math.pi), 0.5) * \
-                 (x_coord * z_coord * (7 * z_coord ^ 2 - 3 * r ^ 2)) / (r ^ 4)
+                 (x_coord * z_coord * (7 * pow(z_coord, 2) - 3 * pow(r, 2))) / (pow(r, 4))
         elif b == 7:
-            basis = (3 / 8) * math.pow(5 / math.pi, 0.5) * ((x_coord ^ 2 - y_coord ^ 2) *
-                                                         (7 * z_coord ^ 2 - r ^ 2)) / (r ^ 4)
+            basis = (3 / 8) * math.pow(5 / math.pi, 0.5) * ((pow(x_coord, 2) - pow(y_coord, 2)) *
+                                                            (7 * pow(z_coord, 2) - pow(r, 2))) / (pow(r, 4))
         elif b == 8:
-            basis = (3 / 4) * math.pow(35 / (2 * math.pi), 0.5) * ((x_coord ^ 2 -
-                                                                 3 * y_coord ^ 2) * x_coord * y_coord) / (r ^ 4)
+            basis = (3 / 4) * math.pow(35 / (2 * math.pi), 0.5) * \
+                    ((pow(x_coord, 2) - 3 * pow(y_coord, 2)) * x_coord * y_coord) / (pow(r, 4))
         elif b == 9:
-            basis = (3 / 16) * math.pow(35 / math.pi, 0.5) * ((x_coord ^ 2) * (x_coord ^ 2 - 3 * y_coord ^ 2) -
-                                                           (y_coord ^ 2) * (3 * x_coord ^ 2 - y_coord ^ 2)) / (r ^ 4)
+            basis = (3 / 16) * math.pow(35 / math.pi, 0.5) * \
+                    ((pow(x_coord, 2)) * (pow(x_coord, 2) - 3 * pow(y_coord, 2)) -(pow(y_coord, 2)) *
+                     (3 * pow(x_coord, 2) - pow(y_coord, 2))) / (pow(r, 4))
         return basis
+
+#    illumination model
+    def get_color(self, reflectance, normal, illumination):
+        illumination = np.reshape(illumination, (3, 9), order='F')
+        summ = 0
+        for i in range(0, 9):
+            summ = summ + illumination[:, i] * self.get_sh_basis_function(normal[0], normal[1], normal[2], i+1)
+        # point wise multiplication
+        color = np.multiply(reflectance, summ)
+        return np.transpose(color)
+
 
 
 def main():
@@ -91,6 +105,7 @@ def main():
 
     vertices = np.asarray(scv_pca_bases["average_shape"])
     ver = np.reshape(vertices, (3, int(vertices.size / 3)), order='F')
+    ref = np.reshape(reflectance, (3, int(reflectance.size / 3)), order='F')
 
     pmod = ParametricMoDecoder(vertices, reflectance)
 
@@ -99,13 +114,14 @@ def main():
 
     projected = np.zeros([2, 53149])
 
-    for i in range(0, 53149):
-        coords_ccs = pmod.transform_wcs2ccs(ver[:, i], inv_rotmat, x["translation"])
-        projected[:, i] = pmod.projection(coords_ccs)
-
-    print(projected.max())
-    plt.scatter(projected[0, ], projected[1, ], s=1)
-    plt.show()
+    pmod.get_color(ref[:, 0], ver[:, 0], x['illumination'])
+    # for i in range(0, 53149):
+    #     coords_ccs = pmod.transform_wcs2ccs(ver[:, i], inv_rotmat, x["translation"])
+    #     projected[:, i] = pmod.projection(coords_ccs)
+    #
+    # print(projected.max())
+    # plt.scatter(projected[0, ], projected[1, ], s=1)
+    # plt.show()
     # plt.plot(vertices[2,: ])
     # plt.show()
     # pmod.projection(v0)
