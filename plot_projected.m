64f36058f9d65a2c0cf174a7cc7e0c27b7f9d74a
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-06-23 15:10:26 +0300

    normals plot

--- plot_projected.m
+++ plot_projected.m
@@ -1,3 +1,6 @@
+clear
+close all
+
 fileID = fopen('color.txt','r');
 A = fscanf(fileID, '%f');
 colors = zeros(3, 53149);
@@ -13,7 +16,8 @@ projected = zeros(2, 53149);
 projected(1,:) = A(1:53149);
 projected(2,:) = A(53150:53149+53149);
 
-scatter(projected(1,:),projected(2,:),100, colors.')
+figure(1)
+scatter(projected(1,:),projected(2,:),5, colors.')
 
 fileID = fopen('reflectance.txt','r');
 A = fscanf(fileID, '%f');
@@ -24,3 +28,14 @@ reflectance(3,:) = A(53150+53149:159447);
 
 figure(2)
 scatter(projected(1,:),projected(2,:),5, reflectance.')
+
+norm(colors - reflectance)
+
+fileID = fopen('normals.txt','r');
+A = fscanf(fileID, '%f');
+normals(1,:) = A(1:53149);
+normals(2,:) = A(53150:53149+53149);
+normals(3,:) = A(53150+53149:159447);
+
+% figure(3)
+% scatter3(normals(1,:), normals(2,:), normals(3,:), 1);
