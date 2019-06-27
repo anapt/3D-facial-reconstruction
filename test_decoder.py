5af155020dfc4c92f8a448523fc549f46ad7ae36
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-06-27 18:03:22 +0300

    minor changes

--- test_decoder.py
+++ test_decoder.py
@@ -1,16 +1,13 @@
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import csv
 
-import math as math
 import parametricMoDecoder as pmd
 import semanticCodeVector as scv
 
 ''' SEMANTIC CODE VECTOR '''
 # MODIFY TO path containing Basel Face model
-path = '/home/anapt/Documents/Thesis - data/data-raw/model2017-1_bfm_nomouth.h5'
+path = './DATASET/model2017-1_bfm_nomouth.h5'
 data = scv.SemanticCodeVector(path)
 x = data.sample_vector()
 cells = data.read_cells()
