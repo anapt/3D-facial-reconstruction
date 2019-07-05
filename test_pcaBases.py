12c18f9dcffada8ed44d06272afc001ec82c3021
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-05 20:17:03 +0300

    pca Bases prep

---
+++ test_pcaBases.py
@@ -0,0 +1,29 @@
+import semanticCodeVector as scv
+import numpy as np
+
+
+def main():
+    # part 1
+    path = './DATASET/model2017-1_bfm_nomouth.h5'
+
+    data = scv.SemanticCodeVector(path)
+    bases = data.read_pca_bases()
+
+    shape_pca = bases['shape_pca']
+    expression_pca = bases['expression_pca']
+    x = data.sample_vector()
+    vertices = data.calculate_coords(x)
+    # sdev = np.std(shape_pca, 0)
+    # print(sdev.shape)
+    # # print(sdev)
+    #
+    # shape_pca = np.multiply(shape_pca, np.transpose(sdev))
+    # print(shape_pca)
+    # print(np.var(shape_pca))
+    # print(np.dot(np.transpose(shape_pca), shape_pca))
+    # print(np.var(expression_pca))
+    # print(np.dot(np.transpose(expression_pca), expression_pca))
+    # diag = variance
+
+
+main()
