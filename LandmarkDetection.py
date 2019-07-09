a257f71536a26e0df134e80b94ab0ae8bf9d83d4
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-09 22:11:04 +0300

    new function that accepts array and not paths

--- LandmarkDetection.py
+++ LandmarkDetection.py
@@ -48,6 +48,41 @@ class LandmarkDetection:
 
             cv2.imwrite(cutout_path, out_face)
 
+            return out_face
+
+    def cutout_mask_array(self, image):
+        out_face = np.zeros_like(image)
+
+        # convert to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        print(gray.shape)
+        print(type(gray))
+        # detect faces in bw image
+        faces = self.detector(np.uint8(gray))
+        for face in faces:
+            landmarks = self.predictor(np.uint8(gray), face)
+            # print(landmarks)
+            shape = face_utils.shape_to_np(landmarks)
+            # initialize mask array
+            # remapped_shape = np.zeros_like(shape)
+            feature_mask = np.zeros((image.shape[0], image.shape[1]))
+
+            # we extract the face
+            remapped_shape = self.face_remap(shape)
+            # get the mask of the face
+            cv2.fillConvexPoly(feature_mask, remapped_shape[0:27], 1)
+
+            mouth = np.array([[shape[60, :], shape[61, :], shape[62, :], shape[63, :], shape[64, :],
+                               shape[65, :], shape[66, :], shape[67, :]]], dtype=np.int32)
+
+            cv2.fillConvexPoly(feature_mask, mouth, 0)
+
+            feature_mask = feature_mask.astype(np.bool)
+
+            out_face[feature_mask] = image[feature_mask]
+
+            return out_face
+
     @staticmethod
     def remove_mouth(image, cutout_path):
         out_face = np.zeros_like(image)
