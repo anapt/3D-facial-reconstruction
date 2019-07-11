a311d960a8a8b05402fabac634b8605272402d6c
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-11 12:12:57 +0300

    cnn add prediction layer

--- testNetwork.py
+++ testNetwork.py
@@ -15,6 +15,37 @@ IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
 # keras.applications.xception.Xception(include_top=True,
 #                                      weights='imagenet', input_tensor=None,
 #                                      input_shape=None, pooling=None, classes=1000)
-base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
-                                               include_top=False,
-                                               weights='imagenet')
+
+# 75 k batch iteration with batch size of 32
+# weight decay of 0.001 l2 regularizer
+BATCH_SIZE = 32
+SHUFFLE_BUFFER_SIZE = 1000
+
+base_model = tf.keras.applications.xception.Xception(include_top=False,
+                                                     weights='imagenet',
+                                                     input_tensor=None,
+                                                     input_shape=IMG_SHAPE)
+base_model.trainable = False
+# base_model.summary()
+
+weights_init = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None)
+prediction_layer = tf.keras.layers.Dense(257, activation=None, use_bias=True,
+                                         kernel_initializer=weights_init, bias_initializer='zeros')
+
+base_learning_rate = 0.01
+# base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+#                                                include_top=False,
+#                                                weights='imagenet')
+
+print("Number of layers in the base model: ", len(base_model.layers))
+model = tf.keras.Sequential([
+  base_model,
+  prediction_layer
+])
+
+model.summary()
+
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
