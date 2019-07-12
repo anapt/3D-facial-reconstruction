c132d398ccb98e4dea92246d94b86f2a58781ded
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-12 22:59:14 +0300

    implementation of Loss Layer

--- LossLayer.py
+++ LossLayer.py
@@ -6,6 +6,7 @@ import semanticCodeVector as scv
 import parametricMoDecoder as pmd
 import LandmarkDetection as ld
 import FaceCropper as fc
+from scipy.linalg import norm
 import cv2
 
 
@@ -33,22 +34,29 @@ class LossLayer:
     def dense_photometric_alignment(self, original_image):
         # TODO original_image has to come from the tf.dataset
         formation = ifl.ImageFormationLayer(self.PATH, self.x)
-        new_image, position = formation.get_reconstructed_image()
-        print(new_image.shape)
-        print(position)
-        print(position.shape)
-        print(type(position))
-        photo_term = original_image[position[0], position[1], :] - new_image
-
+        new_image = formation.get_reconstructed_image()
         plt.imshow(new_image)
         plt.show()
+        photo_term = sum(sum(np.linalg.norm(original_image - new_image, axis=2))) / 53149
+
+        print("photo term", photo_term)
+        return photo_term
+
+    def get_loss(self, original_image):
+        weight_photo = 1.92
+        weight_reg = 2.9e-5
+        # TODO add Sparse Landmark Alignment
+        loss = weight_photo * self.dense_photometric_alignment(original_image) + \
+            weight_reg * self.statistical_regularization_term()
+        print("loss", loss)
+        return loss
 
 
 def main():
     show_result = True
     n = 5
     vector_path = ("./DATASET/semantic/x_%d.txt" % n)
-    image_path = ("./DATASET/images/image_%d.png" % n)
+    image_path = ("./DATASET/images/image_%d.png" % 10)
     vector = np.loadtxt(vector_path)
     # print(vector.shape)
     # vector = np.ones((257, ))
@@ -63,11 +71,15 @@ def main():
     }
 
     ll = LossLayer(vector)
-    sr_term = ll.statistical_regularization_term()
-    print(sr_term)
-    original_image = cv2.imread(image_path)
-    print(type(original_image))
-    ll.dense_photometric_alignment(original_image)
+    # sr_term = ll.statistical_regularization_term()
+    # print(sr_term)
+    original_image = cv2.imread(image_path, 1)
+    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+    plt.imshow(original_image)
+    plt.show()
+    # print(ll.get_loss(original_image))
+    # print(ll.get_loss(original_image))
+    ll.get_loss(original_image)
 
 
 main()
