a8b9b46d296d1eea6208c5a752a9a13945a2fb77
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-13 16:24:02 +0300

    Loss Layer

--- LossLayer.py
+++ LossLayer.py
@@ -2,16 +2,14 @@ import numpy as np
 import matlab.engine
 import matplotlib.pyplot as plt
 import ImageFormationLayer as ifl
-import semanticCodeVector as scv
-import parametricMoDecoder as pmd
-import LandmarkDetection as ld
-import FaceCropper as fc
-from scipy.linalg import norm
+
 import cv2
 
 
 class LossLayer:
     PATH = './DATASET/model2017-1_bfm_nomouth.h5'
+    MAX_FEATURES = 500
+    GOOD_MATCH_PERCENT = 0.15
 
     def __init__(self, vector):
         self.x = {
@@ -35,11 +33,21 @@ class LossLayer:
         # TODO original_image has to come from the tf.dataset
         formation = ifl.ImageFormationLayer(self.PATH, self.x)
         new_image = formation.get_reconstructed_image()
-        plt.imshow(new_image)
-        plt.show()
-        photo_term = sum(sum(np.linalg.norm(original_image - new_image, axis=2))) / 53149
+        # plt.imshow(new_image)
+        # plt.show()
+        # plt.imshow(original_image)
+        # plt.show()
 
-        print("photo term", photo_term)
+        new_image_aligned = self.align_images(new_image, original_image)
+
+        # plt.imshow(new_image_aligned)
+        # plt.show()
+
+        # photo_term = sum(sum(np.linalg.norm(original_image - new_image, axis=2))) / 53149
+        photo_term = sum(sum(np.linalg.norm(original_image - new_image_aligned, axis=2))) / 53149
+
+        # print("photo term", photo_term)
+
         return photo_term
 
     def get_loss(self, original_image):
@@ -48,8 +56,77 @@ class LossLayer:
         # TODO add Sparse Landmark Alignment
         loss = weight_photo * self.dense_photometric_alignment(original_image) + \
             weight_reg * self.statistical_regularization_term()
-        print("loss", loss)
+
+        # print("loss", loss)
+
         return loss
+
+    def align_images(self, new_image, original_image):
+
+        # Convert images to grayscale
+        im1_gray = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
+        im2_gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+
+        # Detect ORB features and compute descriptors.
+        orb = cv2.ORB_create(self.MAX_FEATURES)
+        keypoints1, descriptors1 = orb.detectAndCompute(im1_gray, None)
+        keypoints2, descriptors2 = orb.detectAndCompute(im2_gray, None)
+
+        # Match features.
+        matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+        matches = matcher.match(descriptors1, descriptors2, None)
+
+        # Sort matches by score
+        matches.sort(key=lambda x: x.distance, reverse=False)
+
+        # Remove not so good matches
+        numGoodMatches = int(len(matches) * self.GOOD_MATCH_PERCENT)
+        matches = matches[:numGoodMatches]
+
+        # Draw top matches
+        # imMatches = cv2.drawMatches(new_image, keypoints1, original_image, keypoints2, matches, None)
+        # cv2.imwrite("matches.jpg", imMatches)
+
+        # Extract location of good matches
+        points1 = np.zeros((len(matches), 2), dtype=np.float32)
+        points2 = np.zeros((len(matches), 2), dtype=np.float32)
+
+        for i, match in enumerate(matches):
+            points1[i, :] = keypoints1[match.queryIdx].pt
+            points2[i, :] = keypoints2[match.trainIdx].pt
+
+        # Find homography
+        h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+
+        # Use homography
+        height, width, channels = original_image.shape
+        im1Reg = cv2.warpPerspective(new_image, h, (width, height))
+
+        return im1Reg
+
+    # if __name__ == '__main__':
+    #     # Read reference image
+    #     refFilename = "form.jpg"
+    #     print("Reading reference image : ", refFilename)
+    #     imReference = cv2.imread(refFilename, cv2.IMREAD_COLOR)
+    #
+    #     # Read image to be aligned
+    #     imFilename = "scanned-form.jpg"
+    #     print("Reading image to align : ", imFilename);
+    #     im = cv2.imread(imFilename, cv2.IMREAD_COLOR)
+    #
+    #     print("Aligning images ...")
+    #     # Registered image will be resotred in imReg.
+    #     # The estimated homography will be stored in h.
+    #     imReg, h = align_images(im, imReference)
+    #
+    #     # Write aligned image to disk.
+    #     outFilename = "aligned.jpg"
+    #     print("Saving aligned image : ", outFilename);
+    #     cv2.imwrite(outFilename, imReg)
+    #
+    #     # Print estimated homography
+    #     print("Estimated homography : \n", h)
 
 
 def main():
@@ -75,8 +152,7 @@ def main():
     # print(sr_term)
     original_image = cv2.imread(image_path, 1)
     original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
-    plt.imshow(original_image)
-    plt.show()
+
     # print(ll.get_loss(original_image))
     # print(ll.get_loss(original_image))
     ll.get_loss(original_image)
