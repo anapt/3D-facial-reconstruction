c6b2cc960bf077b4b5295e6ccd5b11ea7c2fb606
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-14 22:21:08 +0300

    class to collect stats in between batches while training

--- CollectBatchStats.py
+++ CollectBatchStats.py
@@ -5,11 +5,11 @@ import tensorflow as tf
 class CollectBatchStats(tf.keras.callbacks.Callback):
     def __init__(self):
         self.batch_losses = []
-        self.batch_acc = []
+        # self.batch_acc = []
 
     def on_train_batch_end(self, batch, logs=None):
         self.batch_losses.append(logs['loss'])
-        self.batch_acc.append(logs['acc'])
+        # self.batch_acc.append(logs['acc'])
         self.model.reset_metrics()
 
 # steps_per_epoch = np.ceil(image_data.samples/image_data.batch_size)
