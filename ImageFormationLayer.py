fc9d500177027ddd3744084f6cd3d9d4de45f37e
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-22 22:57:32 +0300

    image formation layer with new patch function

--- ImageFormationLayer.py
+++ ImageFormationLayer.py
@@ -6,6 +6,7 @@ import SemanticCodeVector as scv
 import ParametricMoDecoder as pmd
 import LandmarkDetection as ld
 import FaceCropper as fc
+import ImagePreprocess as preprocess
 
 
 class ImageFormationLayer(object):
@@ -13,13 +14,15 @@ class ImageFormationLayer(object):
     def __init__(self, vector):
         self.vector = vector
         self.path = './DATASET/model2017-1_bfm_nomouth.h5'
+        self.preprocess = preprocess.ImagePreprocess()
 
     def get_vertices_and_reflectance(self):
         semantic = scv.SemanticCodeVector(self.path)
         vertices = semantic.calculate_coords(self.vector)
+        reflectance = semantic.calculate_reflectance(self.vector)
+
         # read average face cells
         cells = semantic.read_cells()
-        reflectance = semantic.calculate_reflectance(self.vector)
 
         return vertices, reflectance, cells
 
@@ -27,18 +30,14 @@ class ImageFormationLayer(object):
         vertices, reflectance, cells = self.get_vertices_and_reflectance()
         decoder = pmd.ParametricMoDecoder(vertices, reflectance, self.vector, cells)
 
-        image = decoder.get_image_formation()
-
+        formation = decoder.get_image_formation()
         cells = decoder.calculate_cell_depth()
-        cells = cells.tolist()
-        eng = matlab.engine.start_matlab()
-        # coords = image['position']
-        position = image['position'].tolist()
 
-        color = image['color'].tolist()
+        position = formation['position']
+        color = formation['color']
 
         # draw image
-        image = eng.patch_and_show(position, color, cells)
+        image = self.preprocess.patch(position, color, cells)
 
         # get face mask without mouth interior
         cut = ld.LandmarkDetection()
@@ -51,28 +50,27 @@ class ImageFormationLayer(object):
         return cropped_face
 
 
-# def main():
-#     show_result = True
-#     n = 5
-#     path = './DATASET/model2017-1_bfm_nomouth.h5'
-#     vector_path = ("./DATASET/semantic/x_%d.txt" % n)
-#     vector = np.loadtxt(vector_path)
-#
-#     x = {
-#         "shape": vector[0:80, ],
-#         "expression": vector[80:144, ],
-#         "reflectance": vector[144:224, ],
-#         "rotation": vector[224:227, ],
-#         "translation": vector[227:230, ],
-#         "illumination": vector[230:257, ]
-#     }
-#
-#     formation = ImageFormationLayer(path, x)
-#     image = formation.get_reconstructed_image()
-#
-#     if show_result:
-#         plt.imshow(image)
-#         plt.show()
-#
-#
-# main()
+def main():
+    show_result = True
+    n = 10
+    vector_path = ("./DATASET/semantic/x_%d.txt" % n)
+    vector = np.loadtxt(vector_path)
+
+    x = {
+        "shape": vector[0:80, ],
+        "expression": vector[80:144, ],
+        "reflectance": vector[144:224, ],
+        "rotation": vector[224:227, ],
+        "translation": vector[227:230, ],
+        "illumination": vector[230:257, ]
+    }
+
+    formation = ImageFormationLayer(x)
+    image = formation.get_reconstructed_image()
+
+    if show_result:
+        plt.imshow(image)
+        plt.show()
+
+
+main()
