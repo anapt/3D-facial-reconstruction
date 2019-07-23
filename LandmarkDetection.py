5741dba08f0de2cfcf211fba3e3e3e6b0963be9d
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-23 16:23:50 +0300

    Landmarks for loss

--- LandmarkDetection.py
+++ LandmarkDetection.py
@@ -74,36 +74,23 @@ class LandmarkDetection:
             return out_face
 
     def detect_landmarks_for_loss(self, image):
-        out_face = np.zeros_like(image)
-        print("hey")
-        # cv2.imshow("", image)
-        # cv2.waitKey(1000)
-        # convert to grayscale
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        cv2.imshow("", gray)
-        cv2.waitKey(1000)
-        # detect faces in bw image
-        landmarks = self.predictor(gray, dlib.get_rect(gray))
-        print(landmarks)
-        shape = face_utils.shape_to_np(landmarks)
-        print("shape", shape)
-        # # initialize mask array
-        # # remapped_shape = np.zeros_like(shape)
-        # feature_mask = np.zeros((image.shape[0], image.shape[1]))
-        #
-        # # we extract the face
-        # remapped_shape = self.face_remap(shape)
-        # # get the mask of the face
-        # cv2.fillConvexPoly(feature_mask, remapped_shape[0:27], 1)
-        #
-        # # extract the mouth
-        # mouth = np.array([[shape[60, :], shape[61, :], shape[62, :], shape[63, :], shape[64, :],
-        #                    shape[65, :], shape[66, :], shape[67, :]]], dtype=np.int32)
-        # # remove mouth interior
-        # cv2.fillConvexPoly(feature_mask, mouth, 0)
-        #
-        # feature_mask = feature_mask.astype(np.bool)
-        #
-        # out_face[feature_mask] = image[feature_mask]
 
-        return out_face
+        # detect landmarks and transform to image coordinates
+        landmarks = self.predictor(gray, dlib.get_rect(gray))
+        shape = face_utils.shape_to_np(landmarks)
+
+        # keep only 46 landmarks
+        coords = np.array([[shape[17, :], shape[18, :], shape[19, :], shape[20, :], shape[21, :],    # left brow
+                           shape[22, :], shape[23, :], shape[24, :], shape[25, :], shape[26, :],    # right brow
+                           shape[36, :], shape[39, :], shape[42, :], shape[45, :],  # left and right eye limits
+                           shape[27, :], shape[28, :], shape[29, :], shape[30, :], shape[31, :],
+                           shape[32, :], shape[33, :], shape[34, :], shape[35, :],  # nose
+                           shape[48, :], shape[49, :], shape[50, :], shape[51, :], shape[52, :],
+                           shape[53, :], shape[54, :], shape[55, :], shape[56, :], shape[57, :],
+                           shape[58, :], shape[59, :], shape[61, :], shape[62, :], shape[63, :],
+                           shape[65, :], shape[66, :], shape[67, :],    # mouth
+                           shape[6, :], shape[7, :], shape[8, :], shape[9, :], shape[10, :]     # chin
+                            ]], dtype=np.int32)
+
+        return coords
