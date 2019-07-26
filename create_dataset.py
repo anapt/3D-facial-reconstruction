25553ac5a45311e947245eec30866e797fedd35d
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-26 19:56:54 +0300

    minor changes

--- create_dataset.py
+++ create_dataset.py
@@ -6,7 +6,7 @@ def main():
     N = 1000
     preprocess = image_preprocess.ImagePreprocess()
 
-    for n in range(0, 10):
+    for n in range(375, N):
         preprocess.create_image_and_save(n)
         print(n)
 
