a6521f0fda53097f6eceb7d082b9bf2891752ed7
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-27 12:54:29 +0300

    function to prep testing dataset

--- loadDataset.py
+++ loadDataset.py
@@ -29,7 +29,23 @@ def load_and_preprocess_image(path):
     return preprocess_image(image)
 
 
-def load_dataset():
+def load_and_preprocess_image_4d(path):
+    """
+    Reads string path into image string and calls preprocess function to cast into image tensor
+
+    :param path: Tensor("args_0:0", shape=(), dtype=string)
+    :return: Tensor("truediv:0", dtype=float32)
+    """
+    image = tf.io.read_file(path)
+
+    image = tf.image.decode_image(image, channels=3)
+    image = tf.cast(image, dtype=tf.float32)
+    image /= 255.0 - 0.5  # normalize to [-0.5,0.5] range
+    image = tf.reshape(image, shape=[1, 240, 240, 3])
+    return image
+
+
+def load_training_dataset():
     """
     Read images and vectors (from txt files) and zips them together in a Tensorflow Dataset
     Images and vectors should be in different directories
@@ -58,6 +74,48 @@ def load_dataset():
 
     path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
     image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
+
+    vectors = np.zeros((257, len(all_vector_paths)))
+    for n, path in enumerate(all_vector_paths):
+        v = np.loadtxt(path)
+        vectors[:, n] = np.asarray(v)
+
+    vectors_ds = tf.data.Dataset.from_tensor_slices(np.transpose(vectors))
+
+    image_vector_ds = tf.data.Dataset.zip((image_ds, vectors_ds))
+
+    return image_vector_ds
+
+
+def load_testing_dataset():
+    """
+    Read images and vectors (from txt files) and zips them together in a Tensorflow Dataset
+    Images and vectors should be in different directories
+
+    :return: tf.data.Dataset with pairs (Image, Semantic Code Vector)
+    """
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+    data_root = '/home/anapt/PycharmProjects/thesis/DATASET/images/testing'
+    data_root = pathlib.Path(data_root)
+
+    all_image_paths = list(data_root.glob('*'))
+    all_image_paths = [str(path) for path in all_image_paths]
+
+    image_count = len(all_image_paths)
+    print("Dataset containg %d pairs of Images and Vectors." % image_count)
+
+    sem_root = '/home/anapt/PycharmProjects/thesis/DATASET/semantic/testing'
+    sem_root = pathlib.Path(sem_root)
+
+    all_vector_paths = list(sem_root.glob('*'))
+    all_vector_paths = [str(path) for path in all_vector_paths]
+
+    all_image_paths.sort()
+    all_vector_paths.sort()
+
+    path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
+    image_ds = path_ds.map(load_and_preprocess_image_4d, num_parallel_calls=AUTOTUNE)
 
     vectors = np.zeros((257, len(all_vector_paths)))
     for n, path in enumerate(all_vector_paths):
