7c3cc3eefb3609ca813bcac4954195341e67bf53
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-28 00:09:26 +0300

    moved to testers

--- testers/test_inheritance.py
+++ testers/test_inheritance.py
@@ -1,9 +1,2 @@
-import numpy as np
-import matlab.engine
-import matplotlib.pyplot as plt
 
-import SemanticCodeVector as scv
-import ParametricMoDecoder as pmd
-import LandmarkDetection as ld
-import FaceCropper as fc
 
