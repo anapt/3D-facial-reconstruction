7ae0be8eff6d1129f984d4e09698d4c0ed6fa68c
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-30 18:17:47 +0300

    clean up

--- CollectBatchStats.py
+++ CollectBatchStats.py
@@ -11,23 +11,3 @@ class CollectBatchStats(tf.keras.callbacks.Callback):
         self.batch_losses.append(logs['loss'])
         # self.batch_acc.append(logs['acc'])
         self.model.reset_metrics()
-
-# steps_per_epoch = np.ceil(image_data.samples/image_data.batch_size)
-#
-# batch_stats_callback = CollectBatchStats()
-#
-# history = model.fit(image_data, epochs=2,
-#                     steps_per_epoch=steps_per_epoch,
-#                     callbacks = [batch_stats_callback])
-#
-# plt.figure()
-# plt.ylabel("Loss")
-# plt.xlabel("Training Steps")
-# plt.ylim([0,2])
-# plt.plot(batch_stats_callback.batch_losses)
-#
-# plt.figure()
-# plt.ylabel("Accuracy")
-# plt.xlabel("Training Steps")
-# plt.ylim([0,1])
-# plt.plot(batch_stats_callback.batch_acc)
