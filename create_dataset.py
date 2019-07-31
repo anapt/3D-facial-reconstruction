d7a4f573d56424165416b309ff32e0cf484fe14e
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-07-31 17:21:52 +0300

    minor changes

--- create_dataset.py
+++ create_dataset.py
@@ -3,10 +3,10 @@ import ImagePreprocess as image_preprocess
 
 def main():
     # Number of images to create
-    N = 100
+    N = 10
     preprocess = image_preprocess.ImagePreprocess()
 
-    for n in range(25, N+1):
+    for n in range(0, N+1):
         preprocess.create_image_and_save(n)
         print(n)
 
