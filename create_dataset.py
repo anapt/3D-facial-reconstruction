d7d32b1cfa619f1deefa6302defc374aa89cf6db
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-08-06 19:37:32 +0300

    50k images

--- create_dataset.py
+++ create_dataset.py
@@ -4,10 +4,10 @@ import time
 
 def main():
     # Number of images to create
-    N = 20000
+    N = 50000
     preprocess = image_preprocess.ImagePreprocess()
 
-    for n in range(19030, N):
+    for n in range(20000, N):
         start = time.time()
         preprocess.create_image_and_save(n)
         # print("Time passed:", time.time() - start)
