65dd87251e1ea00a1887bded5ff0b0b30cbbc852
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-08-20 15:38:21 +0300

    into function

--- prediction_plots.py
+++ prediction_plots.py
@@ -5,6 +5,46 @@ from FaceNet3D import FaceNet3D as Helpers
 helper = Helpers()
 
 
+def prediction_plots(x_true, x, save_figs):
+    plt.figure()
+    plt.title('Shape')
+    plt.plot(x['shape'], color='red', marker='o', linestyle='None')
+    plt.plot(x_true['shape'], color='blue', marker='+', linestyle='None', alpha=1)
+    if save_figs:
+        plt.savefig('./tests/exp7/shape.png')
+    plt.show()
+
+    # plot expression, red dots are predicted values
+    # blue crosses are ground truth
+    plt.figure()
+    plt.title('Expression')
+    plt.plot(x['expression'], color='red', marker='D', linestyle='None')
+    plt.plot(x_true['expression'], color='blue', marker='d', linestyle='None', alpha=1)
+    if save_figs:
+        plt.savefig('./tests/exp7/expression.png')
+    plt.show()
+
+    # plot shape, red dots are predicted values
+    # blue crosses are ground truth
+    plt.figure()
+    plt.title('Color')
+    plt.plot(x['color'], color='red', marker='h', linestyle='None', alpha=0.5)
+    plt.plot(x_true['color'], color='blue', marker='H', linestyle='None', alpha=0.5)
+    if save_figs:
+        plt.savefig('./tests/exp7/color.png')
+    plt.show()
+
+    # plot shape, red dots are predicted values
+    # blue crosses are ground truth
+    plt.figure()
+    plt.title('Rotation')
+    plt.plot(x['rotation'], color='red', marker='o', linestyle='None')
+    plt.plot(x_true['rotation'], color='blue', marker='+', linestyle='None', alpha=1)
+    if save_figs:
+        plt.savefig('./tests/exp7/rotation.png')
+    plt.show()
+
+
 def main():
     x_true = np.loadtxt(helper.sem_root + 'training/x_{:06}.txt'.format(1))
     x_true = helper.vector2dict(x_true)
@@ -58,4 +98,4 @@ def main():
 
 
 
-main()
+# main()
