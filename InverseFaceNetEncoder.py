e5f8220cdecc513b6249f54daafee0617343c9b2
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-08-21 20:27:48 +0300

    clean up

--- InverseFaceNetEncoder.py
+++ InverseFaceNetEncoder.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 from keras import backend as K
+import numpy as np
 from FaceNet3D import FaceNet3D as Helpers
 from SemanticCodeVector import SemanticCodeVector
 
@@ -22,6 +23,12 @@ class InverseFaceNetEncoder(Helpers):
 
         self.data = SemanticCodeVector()
         self.shape_std, self.color_std, self.expression_std = self.data.get_bases_std()
+        self.scale_shape = self.shape_dim / np.sum(self.shape_std)
+        self.scale_color = self.color_dim / np.sum(self.color_std)
+        self.scale_expression = self.expression_dim / np.sum(self.expression_std)
+
+        self.early = tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0, patience=2, verbose=1,
+                                                      mode='min', baseline=None, restore_best_weights=True)
 
     def build_model(self):
         """
@@ -30,25 +37,41 @@ class InverseFaceNetEncoder(Helpers):
         :return: Keras.model()
         """
 
-        # Create the base model from the pre-trained model XCEPTION
-        # base_model = tf.keras.applications.xception.Xception(include_top=False,
-        #                                                      weights='imagenet',
-        #                                                      input_tensor=None,
-        #                                                      input_shape=self.IMG_SHAPE,
-        #                                                      pooling='avg')
-        base_model = tf.keras.applications.vgg16.VGG16(include_top=False,
-                                                       weights='imagenet',
-                                                       input_shape=self.IMG_SHAPE,
-                                                       pooling='avg')
-        base_model.trainable = False
+        base_model = tf.keras.applications.resnet50.ResNet50(include_top=False,
+                                                             weights='imagenet',
+                                                             input_tensor=None,
+                                                             # input_shape=self.IMG_SHAPE,
+                                                             pooling='avg')
+
+        # for layer in base_model.layers[:]:
+        #     layer.trainable = True
+        """
+        
+        
+        
+        # Freeze the layers which you don't want to train. Here I am freezing the first 5 layers.
+        
+        
+        #Adding custom Layers 
+        x = model.output
+        x = Flatten()(x)
+        x = Dense(1024, activation="relu")(x)
+        x = Dropout(0.5)(x)
+        x = Dense(1024, activation="relu")(x)
+        predictions = Dense(16, activation="softmax")(x)
+        
+        # creating the final model 
+        model_final = Model(input = model.input, output = predictions)
+        
+        # compile the model 
+        model_final.compile(loss = "categorical_crossentropy", optimizer = optimizers.SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
+        
+                """
+
+        base_model.trainable = True
         # base_model.summary()
 
-        # weights_init = tf.constant(weights_init, shape=(self.scv_length,), dtype=tf.float32)
         weights_init = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None)
-
-        # Create global average pooling layer
-        # global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
-        # global_average_layer = tf.keras.layers.GlobalMaxPooling2D()
 
         # Create prediction layer
         prediction_layer = tf.keras.layers.Dense(self.scv_length, activation=None, use_bias=True,
@@ -56,25 +79,26 @@ class InverseFaceNetEncoder(Helpers):
                                                  # kernel_regularizer=tf.keras.regularizers.l2(self.WEIGHT_DECAY)
                                                  )
 
-        # Code to check compatibility of dimensions
-        # for image_batch, label_batch in keras_ds.take(1):
-        #     pass
-        # print('image batch shape ', image_batch.shape)
-        # feature_batch = base_model(image_batch)
-        # print('feature batch shape', feature_batch.shape)
-        # feature_batch_average = global_average_layer(feature_batch)
-        # print(feature_batch_average.shape)
-        # prediction_batch = prediction_layer(feature_batch_average)
-        # print(prediction_batch.shape)
-
-        # print("Number of layers in the base model: ", len(base_model.layers))
-
         # Stack model layers
         model_o = tf.keras.Sequential([
             base_model,
             prediction_layer
         ])
-
+        """
+        Model: "sequential"
+        _________________________________________________________________
+        Layer (type)                 Output Shape              Param #   
+        == == == == == == == == == == == == == == == == == == == == == ==
+        resnet50(Model)             (None, 2048)                23587712
+        _________________________________________________________________
+        dense(Dense)                (None, 231)                 473319
+        == == == == == == == == == == == == == == == == == == == == == == 
+        Total params: 24,061,031
+        Trainable params: 24,007,911
+        Non - trainable params: 53,120 (from resnet50)
+        _________________________________________________________________
+        """
+        model_o.summary()
         return model_o
 
     def model_space_parameter_loss(self, y):
@@ -87,49 +111,31 @@ class InverseFaceNetEncoder(Helpers):
         """
         std_shape = tf.constant(self.shape_std, dtype=tf.float32)
         std_shape = tf.compat.v1.reshape(std_shape, shape=(self.shape_dim,))
-        # std_shape = K.tile(std_shape, self.BATCH_SIZE)
 
-        # # weight
-        shape = tf.math.scalar_mul(14.4, std_shape, name='shape_std')
+        # weight
+        shape = tf.math.scalar_mul(self.scale_shape, std_shape, name='shape_std')
 
         std_expression = tf.constant(self.expression_std, dtype=tf.float32)
         std_expression = tf.compat.v1.reshape(std_expression, shape=(self.expression_dim,))
-        # std_expression = K.tile(std_expression, self.BATCH_SIZE)
 
-        # # weight
-        expression = tf.math.scalar_mul(7.84, std_expression, name='expression_std')
+        # weight
+        expression = tf.math.scalar_mul(self.scale_expression, std_expression, name='expression_std')
 
         std_color = tf.constant(self.color_std, dtype=tf.float32)
         std_color = tf.compat.v1.reshape(std_color, shape=(self.color_dim,))
-        # std_color = K.tile(std_color, self.BATCH_SIZE)
 
         # weight
-        color = tf.math.scalar_mul(8.13, std_color, name='shape_std')
+        color = tf.math.scalar_mul(self.scale_color, std_color, name='shape_std')
 
-        # with tf.device('/device:GPU:1'):
-        # shape = tf.constant(15, shape=(1,), dtype=tf.float32)
-        # shape = K.tile(shape, self.shape_dim)
-        #
-        # expression = tf.constant(15, shape=(1,), dtype=tf.float32)
-        # # expression2 = tf.constant(0, shape=(1,), dtype=tf.float32)
-        # expression = K.tile(expression, self.expression_dim)
-        #
-        # color = tf.constant(10, shape=(1,), dtype=tf.float32)
-        # color = K.tile(color, self.color_dim)
-        #
-        rotation = tf.constant(52.5, shape=(1,), dtype=tf.float32)
+        rotation = tf.constant(1, shape=(1,), dtype=tf.float32)
         rotation = K.tile(rotation, self.rotation_dim)
 
         sigma = tf.compat.v1.concat([shape, expression, color, rotation],
                                     axis=0)
 
-        sigma = tf.linalg.tensor_diag(sigma)
+        alpha = tf.math.multiply(sigma, y)
 
-        alpha = tf.linalg.matmul(sigma, y, transpose_b=True)
-
-        beta = tf.linalg.matmul(alpha, alpha, transpose_a=True)
-
-        # loss = K.mean(beta, axis=-1)
+        beta = K.mean(alpha)
 
         return beta
 
@@ -142,7 +148,8 @@ class InverseFaceNetEncoder(Helpers):
 
         def custom_loss(y_true, y_pred):
             # with tf.device('/device:GPU:1'):
-            y = tf.math.abs(tf.math.subtract(y_pred, y_true, name='pred_minus_true'))
+            # y = tf.math.abs(tf.math.subtract(y_pred, y_true, name='pred_minus_true'))
+            y = K.square(y_pred - y_true)
 
             # Model Space Parameter Loss
             model_loss = model_space_loss(y)
@@ -156,6 +163,13 @@ class InverseFaceNetEncoder(Helpers):
         self.model.compile(optimizer=tf.keras.optimizers.Adadelta(lr=self.BASE_LEARNING_RATE,
                                                                   rho=0.95, epsilon=None, decay=self.WEIGHT_DECAY),
                            loss=self.loss_func,
-                           # loss=tf.keras.losses.mean_absolute_error,
+                           # loss=tf.keras.losses.mean_squared_error,
                            metrics=[tf.keras.losses.mean_squared_error, tf.keras.losses.mean_absolute_error])
         print('Model Compiled!')
+
+
+def main():
+    net = InverseFaceNetEncoder().compile()
+
+
+# main()
--- old_encoder.py
+++ old_encoder.py
@@ -23,9 +23,6 @@ class InverseFaceNetEncoder(Helpers):
         self.data = SemanticCodeVector()
         self.shape_std, self.color_std, self.expression_std = self.data.get_bases_std()
 
-        self.early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=1,
-                                                      mode='min', baseline=None, restore_best_weights=True)
-
     def build_model(self):
         """
          Create a Keras model
@@ -33,41 +30,30 @@ class InverseFaceNetEncoder(Helpers):
         :return: Keras.model()
         """
 
-        base_model = tf.keras.applications.resnet50.ResNet50(include_top=False,
+        # Create the base model from the pre-trained model XCEPTION
+        base_model = tf.keras.applications.xception.Xception(include_top=False,
                                                              weights='imagenet',
                                                              input_tensor=None,
-                                                             # input_shape=self.IMG_SHAPE,
+                                                             input_shape=self.IMG_SHAPE,
                                                              pooling='avg')
-
-        # for layer in base_model.layers[:]:
-        #     layer.trainable = True
-        """
-        
-        
-        
-        # Freeze the layers which you don't want to train. Here I am freezing the first 5 layers.
-        
-        
-        #Adding custom Layers 
-        x = model.output
-        x = Flatten()(x)
-        x = Dense(1024, activation="relu")(x)
-        x = Dropout(0.5)(x)
-        x = Dense(1024, activation="relu")(x)
-        predictions = Dense(16, activation="softmax")(x)
-        
-        # creating the final model 
-        model_final = Model(input = model.input, output = predictions)
-        
-        # compile the model 
-        model_final.compile(loss = "categorical_crossentropy", optimizer = optimizers.SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
-        
-                """
-
+        # base_model = tf.keras.applications.resnet50.ResNet50(include_top=False,
+        #                                                     weights='imagenet',
+        #                                                     input_tensor=None,
+        #                                                     input_shape=self.IMG_SHAPE,
+        #                                                     pooling='avg')
+        # base_model = tf.keras.applications.vgg16.VGG16(include_top=False,
+        #                                                weights='imagenet',
+        #                                                input_shape=self.IMG_SHAPE,
+        #                                                pooling='avg')
         base_model.trainable = True
         # base_model.summary()
 
+        # weights_init = tf.constant(weights_init, shape=(self.scv_length,), dtype=tf.float32)
         weights_init = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01, seed=None)
+
+        # Create global average pooling layer
+        # global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+        # global_average_layer = tf.keras.layers.GlobalMaxPooling2D()
 
         # Create prediction layer
         prediction_layer = tf.keras.layers.Dense(self.scv_length, activation=None, use_bias=True,
@@ -75,26 +61,25 @@ class InverseFaceNetEncoder(Helpers):
                                                  # kernel_regularizer=tf.keras.regularizers.l2(self.WEIGHT_DECAY)
                                                  )
 
+        # Code to check compatibility of dimensions
+        # for image_batch, label_batch in keras_ds.take(1):
+        #     pass
+        # print('image batch shape ', image_batch.shape)
+        # feature_batch = base_model(image_batch)
+        # print('feature batch shape', feature_batch.shape)
+        # feature_batch_average = global_average_layer(feature_batch)
+        # print(feature_batch_average.shape)
+        # prediction_batch = prediction_layer(feature_batch_average)
+        # print(prediction_batch.shape)
+
+        # print("Number of layers in the base model: ", len(base_model.layers))
+
         # Stack model layers
         model_o = tf.keras.Sequential([
             base_model,
             prediction_layer
         ])
-        """
-        Model: "sequential"
-        _________________________________________________________________
-        Layer (type)                 Output Shape              Param #   
-        == == == == == == == == == == == == == == == == == == == == == ==
-        resnet50(Model)             (None, 2048)                23587712
-        _________________________________________________________________
-        dense(Dense)                (None, 231)                 473319
-        == == == == == == == == == == == == == == == == == == == == == == 
-        Total params: 24,061,031
-        Trainable params: 24,007,911
-        Non - trainable params: 53,120 (from resnet50)
-        _________________________________________________________________
-        """
-        model_o.summary()
+
         return model_o
 
     def model_space_parameter_loss(self, y):
@@ -162,8 +147,8 @@ class InverseFaceNetEncoder(Helpers):
 
         def custom_loss(y_true, y_pred):
             # with tf.device('/device:GPU:1'):
-            y = tf.math.abs(tf.math.subtract(y_pred, y_true, name='pred_minus_true'))
-            # y = tf.math.subtract(y_pred, y_true, name='pred_minus_true')
+            # y = tf.math.abs(tf.math.subtract(y_pred, y_true, name='pred_minus_true'))
+            y = tf.math.subtract(y_pred, y_true, name='pred_minus_true')
 
             # Model Space Parameter Loss
             model_loss = model_space_loss(y)
@@ -180,10 +165,3 @@ class InverseFaceNetEncoder(Helpers):
                            # loss=tf.keras.losses.mean_absolute_error,
                            metrics=[tf.keras.losses.mean_squared_error, tf.keras.losses.mean_absolute_error])
         print('Model Compiled!')
-
-
-def main():
-    net = InverseFaceNetEncoder().compile()
-
-
-main()
