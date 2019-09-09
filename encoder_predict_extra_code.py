424d3d5bc29d9692f00336643fdfcf5e59a51756
Pachni Tsitiridou A <ana.pachni.tsitiridou@gmail.com>
2019-09-09 22:44:23 +0300

    minor changes

--- encoder_predict_extra_code.py
+++ encoder_predict_extra_code.py
@@ -12,30 +12,30 @@ def main():
     # path = net.bootstrapping_path + 'MUG/'
     path = "./DATASET/images/validation/"
     data_root = pathlib.Path(path)
-    all_image_paths = list(data_root.glob('000010.png'))
+    all_image_paths = list(data_root.glob('image*.png'))
     all_image_paths = [str(path) for path in all_image_paths]
     all_image_paths.sort()
     print(all_image_paths)
-    all_image_paths = all_image_paths[0:1]
-    for n, path in enumerate(all_image_paths):
-
-        # net.evaluate_model()
-
-        x = net.model_predict(path)
-        # np.savetxt(net.bootstrapping_path + 'test_loss/x_120_{:06}.txt'.format(n), x)
-        np.savetxt("./DATASET/images/validation/p500plus/x_{:06}.txt".format(n), x)
-        x = net.vector2dict(x)
-
-        x_true = np.loadtxt(net.sem_root + 'validation/x_{:06}.txt'.format(n))
-        # x_true = np.zeros((net.scv_length, ))
-        x_true = net.vector2dict(x_true)
-
-        prediction_plots(x_true, x, save_figs=False)
-
-        image = net.calculate_decoder_output(x)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # cv2.imwrite(net.bootstrapping_path + 'test_loss/image_120_{:06}.png'.format(n), image)
-        cv2.imwrite("./DATASET/images/validation/p500plus/pimage_{:06}.png".format(n), image)
+    all_image_paths = all_image_paths[0:10]
+    # for n, path in enumerate(all_image_paths):
+    #
+    #     # net.evaluate_model()
+    #
+    #     x = net.model_predict(path)
+    #     # np.savetxt(net.bootstrapping_path + 'test_loss/x_120_{:06}.txt'.format(n), x)
+    #     np.savetxt("./DATASET/images/validation/p500plus/x_{:06}.txt".format(n), x)
+    #     x = net.vector2dict(x)
+    #
+    #     x_true = np.loadtxt(net.sem_root + 'validation/x_{:06}.txt'.format(n))
+    #     # x_true = np.zeros((net.scv_length, ))
+    #     x_true = net.vector2dict(x_true)
+    #
+    #     prediction_plots(x_true, x, save_figs=False)
+    #
+    #     image = net.calculate_decoder_output(x)
+    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #     # cv2.imwrite(net.bootstrapping_path + 'test_loss/image_120_{:06}.png'.format(n), image)
+    #     cv2.imwrite("./DATASET/images/validation/p500plus/pimage_{:06}.png".format(n), image)
     # x = np.loadtxt("./DATASET/images/validation/p240/xx_{:06}.txt".format(43))
     # x_target = np.loadtxt("./DATASET/images/validation/p240/xx_{:06}.txt".format(2))
     # x = net.vector2dict(x)
